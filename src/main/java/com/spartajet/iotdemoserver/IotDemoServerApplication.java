9a15c2767612dc630d1b45ec71940710b57ead7a
spartajet <spartajet.guo@gmail.com>
2018-09-03 07:58:34 +0800

    Initial commit

---
+++ src/main/java/com/spartajet/iotdemoserver/IotDemoServerApplication.java
@@ -0,0 +1,12 @@
+package com.spartajet.iotdemoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotDemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotDemoServerApplication.class, args);
+	}
+}
